03d8beb6b428225e303ecfaefc9574701dcb675d
xzw <1272117264@qq.com>
2018-11-24 16:14:03 +0800

    使用note操作数据库

---
+++ model/note.js
@@ -0,0 +1,52 @@
+var Sequelize = require('sequelize');
+var path=require('path')
+//创建数据库
+const sequelize = new Sequelize(undefined,undefined,undefined, {
+  host: 'localhost',
+  dialect: 'sqlite',
+  storage: path.join(__dirname,'../database/database.sqlite')
+});
+//测试数据库连接情况
+/*
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+*/
+
+//创建一个表结构
+  var Note= sequelize.define('note', {
+    text: {
+      type: Sequelize.STRING
+    },
+    //不需要ID，因为会自动给ID
+  });
+
+module.exports.Note=Note
+
+
+
+
+
+
+/*
+  Note.sync().then(() => {
+    // Table created
+    return Note.create({
+      text: 'hello'
+    });
+  });
+
+*/
+//具体的复杂情况看文档http://docs.sequelizejs.com/manual/installation/getting-started.html
+//raw:true 获取原始数据，去除不必要的信息
+/** 
+Note.findAll({raw:true,where:{id:2}}).then(note => {
+    console.log(note)
+  })
+*/ 
