4884b03774964acba154aa4e487e1879b1accefc
xzw <1272117264@qq.com>
2018-12-20 20:36:50 +0800

    addNote模块化

--- src/js/mod/addNote.js
+++ src/js/mod/addNote.js
@@ -1,23 +1,25 @@
+require('less/addNote.less')
 var NoteManager=require('mod/note-manager.js').NoteManager
 var Event=require('mod/event.js')
-var WaterFall=require('mod/waterfall.js')
-var Note=require('mod/note.js').Note
-var currentColor='#AD1E1E'
+var currentColor='#FF666A'
 var date=''
-
 var addNote=function (){
-    var data=$('.mask-content').text()
     var temp=new Date()
     date=temp.toString().slice(4,15)
-    //if(data==='input here') $('.mask-content').html('');
     $('.add-note').on('click',function(){
         $('.mask').css({'display':'flex'})     
     })
     $('.exit').on('click',function(){
         $('.mask').css({'display':'none'})
     })
+    $('.mask-content').on('keydown',function(e){ //防止div输入换行出现bug
+        if (e.keyCode === 13) {
+           document.execCommand('insertHTML', false, '<br><br>');
+          return false;
+        }
+    })
     $('.confirm').on('click',function(){
-        var userInput=$('.mask-content').text()
+        var userInput=$('.mask-content').html()
         $('.mask').css({'display':'none'})
         $('.mask-content').html('');
         NoteManager.add({  // 在页面添加一个note
@@ -28,13 +30,18 @@ var addNote=function (){
         Event.fire('confirm',{  //把新建note的数据上传到服务器
             context:userInput,
             color:currentColor,
-            date:date})
+            date:date
+        })         
         Event.fire('waterfall')
     })
     $('.color').each(function(){
         let curLi=this
-        $(curLi).on('click',function(){  
-            currentColor=$(this).css('background-color')  
+        let $title=$('.mask-header')
+        $(curLi).on('click',function(){ 
+            $(this).addClass('selected').siblings().removeClass('selected')//.remove()
+            currentColor=$(this).css('background-color')
+            $title.css({color:currentColor})
+              
         })
     })  
 }
