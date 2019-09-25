5155142aa172d269b161c50d314823c69aefa047
jenvyxu <1272117264@qq.com>
2019-09-25 19:24:48 +0800

    修改内容居中的样式

--- src/js/mod/waterfall.js
+++ src/js/mod/waterfall.js
@@ -1,39 +1,42 @@
-var WaterFall=(function(){
+var WaterFall = (function() {
     var $ct
     var $items
-    function render($c){
-        $ct=$c
-        $items=$ct.children()
-        var nodeWidth=$items.outerWidth(true),
-            colNum=parseInt(($(window).width()-60)/nodeWidth),//计算有多少列 //左右margin 宽度是60
-            colSumHeight=[]
-            for(var i=0;i<colNum;i++){//设置每一列的初始高度为0
-                colSumHeight.push(0)
-            }
-            $items.each(function(){
-                var $cur=$(this)
-                var idx=0,minSumHeight=colSumHeight[0]
-                for(var i=0;i<colSumHeight.length;i++){//找到最矮的那一列
-                    if(colSumHeight[i]<minSumHeight){
-                        idx=i
-                        minSumHeight=colSumHeight[i]
-                    }
+
+    function render($c) {
+        $ct = $c
+        $items = $ct.children()
+        var nodeWidth = $items.outerWidth(true),
+            colNum = parseInt(($(window).width()) / nodeWidth), //计算有多少列 //左右margin 宽度是60
+            colSumHeight = []
+        $ct.width(colNum * nodeWidth)
+        for (var i = 0; i < colNum; i++) { //设置每一列的初始高度为0
+            colSumHeight.push(0)
+        }
+        $items.each(function() {
+            var $cur = $(this)
+            var idx = 0,
+                minSumHeight = colSumHeight[0]
+            for (var i = 0; i < colSumHeight.length; i++) { //找到最矮的那一列
+                if (colSumHeight[i] < minSumHeight) {
+                    idx = i
+                    minSumHeight = colSumHeight[i]
                 }
-                $cur.css({
-                    //把新的便签放在高度最低的那列下面
-                    left:nodeWidth*idx,
-                    top:minSumHeight,
-                })
-                colSumHeight[idx]=$cur.outerHeight(true)+colSumHeight[idx]
+            }
+            $cur.css({
+                //把新的便签放在高度最低的那列下面
+                left: nodeWidth * idx,
+                top: minSumHeight,
             })
+            colSumHeight[idx] = $cur.outerHeight(true) + colSumHeight[idx]
+        })
     }
-    $(window).on('resize',function(){
+    $(window).on('resize', function() {
         render($ct)
     })
 
     return {
-        init:render
+        init: render
     }
 })()
 
-module.exports=WaterFall
+module.exports = WaterFall
