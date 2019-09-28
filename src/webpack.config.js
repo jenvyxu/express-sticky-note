9d17557f68bab425874565e6bf63d7c08ac1b506
jenvyxu <1272117264@qq.com>
2019-09-28 17:15:18 +0800

    修改webpack配置文件

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,47 +1,48 @@
-var webpack=require('webpack')
+var webpack = require('webpack')
 var path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, "js/app/index.js"),
-  output: {
-    filename: 'index.js',
-    path: path.join(__dirname, '../public/js')
-	},
-	devtool: 'source-map',
-	watch:'true',
-	watchOptions: {
-    aggregateTimeout: 300,
-		ignored: '../node_modules/',
-  },
-  resolve:{
-  	alias:{
-  		jquery: path.join(__dirname,"js/lib/jquery-3.3.1.min.js"),
-  		mod: path.join(__dirname,"js/mod"),
-  		less: path.join(__dirname,"less")
-  	}
-  },
-  plugins:[
-   		new webpack.ProvidePlugin({
-   			$:"jquery"
-   		})
-  ],
-  module:{
-  	rules:[
+    entry: path.join(__dirname, "js/app/index.js"),
+    output: {
+        filename: 'index.js',
+        path: path.join(__dirname, '../public/js')
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 300,
+        ignored: '../node_modules/',
+    },
+    resolve: {
+        alias: {
+            jquery: path.join(__dirname, "js/lib/jquery-3.3.1.min.js"),
+            mod: path.join(__dirname, "js/mod"),
+            less: path.join(__dirname, "less")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery"
+        })
+    ],
+    module: {
+        rules: [
 
-  	{
-  		test:/\.less$/,
-  		use:["style-loader","css-loader","less-loader"]
-		},	
-		{
-		   test: /\.(png|svg|jpg|gif)$/,
-		    use: {
- 					loader:'file-loader',			  
-				 	options: {
-						outputPath: '../images/'
-					}
-				}
-		     
-		  }	
-		]
-  }
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: '../images/'
+                    }
+                }
+
+            }
+        ]
+    }
 };
